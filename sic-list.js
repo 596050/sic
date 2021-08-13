1bf17b9fbd1e8175003995e8cc3a2d370ff8155a
Thomas Price <thomas.price@moneysavingexpert.com>
2021-08-13 14:11:53 +0100

    ISSUE-1 Add divisions.json

--- sic-list.js
+++ sic-list.js
@@ -1,11 +1,13 @@
-'use strict';
+"use strict";
 
-import data from './data.json';
+import data from "./data.json";
+import divisions from "./divisions.json";
 
 const descriptionMap = {};
 const codeMap = {};
+const divisionMap = {};
 
-const mapData = item => {
+const mapData = (item) => {
   descriptionMap[item.description.toLowerCase()] = item.code;
   codeMap[item.code] = item.description;
 };
@@ -13,26 +15,30 @@ const mapData = item => {
 data.forEach(mapData);
 
 // Codes
-export const getCode = description => descriptionMap[description.toLowerCase()];
+export const getCode = (description) =>
+  descriptionMap[description.toLowerCase()];
 
-export const getCodes = () => data.map(item => item.code);
+export const getCodes = () => data.map((item) => item.code);
 
 export const getCodeList = () => codeMap;
 
 // Descriptions
-export const getDescription = code => codeMap[String(code)];
+export const getDescription = (code) => codeMap[String(code)];
 
-export const getDescriptions = () => data.map(item => item.description);
+export const getDescriptions = () => data.map((item) => item.description);
 
 export const getDescriptionList = () => descriptionMap;
+
+// Divisions
+export const getDivisions = () => divisions;
 
 // General
 export const getData = () => data;
 
-export const overwrite = sicArray => {
+export const overwrite = (sicArray) => {
   if (!sicArray || !sicArray.length) return;
-  sicArray.forEach(sic => {
-    const foundIndex = data.findIndex(item => item.code === String(sic.code));
+  sicArray.forEach((sic) => {
+    const foundIndex = data.findIndex((item) => item.code === String(sic.code));
     if (foundIndex > -1) {
       data[foundIndex] = sic;
       mapData(sic);
